dbb4646d429a8f692c516d3d28b9981c928cee2f
Daniel Newton <djpnewton@gmail.com>
2011-05-26 20:21:42 +1200

    * fix missing dependancy on docutils

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(name='kotti_rstdocument',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires=['Kotti'],
+      install_requires=['Kotti', 'docutils'],
       tests_require=['nose', 'coverage'],
       )
